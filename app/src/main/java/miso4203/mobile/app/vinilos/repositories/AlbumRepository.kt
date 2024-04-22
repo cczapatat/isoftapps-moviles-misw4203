132810328061bb23997b0004ef2180c2d16bf5cb
cristian.parada <cristian.parada@COC02Y30G9JGH7.local>
2024-04-22 09:43:50 -0500

    update logic to add album repository

---
+++ app/src/main/java/miso4203/mobile/app/vinilos/repositories/AlbumRepository.kt
@@ -0,0 +1,16 @@
+package miso4203.mobile.app.vinilos.repositories
+
+import android.app.Application
+import com.android.volley.VolleyError
+import miso4203.mobile.app.vinilos.models.Album
+import miso4203.mobile.app.vinilos.network.NetworkServiceAdapter
+
+class AlbumRepository (val application: Application){
+    fun refreshData(callback: (List<Album>)->Unit, onError: (VolleyError)->Unit) {
+        NetworkServiceAdapter.getInstance(application).getAlbums({
+            callback(it)
+        },{
+            onError
+        })
+    }
+}
