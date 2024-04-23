9ab4acbfcbf3b6234849ceaac519471901209292
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2024-04-23 18:42:46 -0500

    feature: Add recycler views for performer and track album details and layouts

--- app/src/main/java/miso4203/mobile/app/vinilos/ui/album_detail/AlbumDetailFragment.kt
+++ app/src/main/java/miso4203/mobile/app/vinilos/ui/album_detail/AlbumDetailFragment.kt
@@ -1,8 +1,6 @@
 package miso4203.mobile.app.vinilos.ui.album_detail
 
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +8,14 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.MemoryPolicy
 import com.squareup.picasso.Picasso
-import miso4203.mobile.app.vinilos.R
 import miso4203.mobile.app.vinilos.databinding.FragmentAlbumDetailBinding
+import miso4203.mobile.app.vinilos.ui.adapters.PerformerAdapter
+import miso4203.mobile.app.vinilos.ui.adapters.TrackAdapter
+
 
 class AlbumDetailFragment : Fragment() {
     private var _binding: FragmentAlbumDetailBinding? = null
@@ -45,6 +46,7 @@ class AlbumDetailFragment : Fragment() {
             this,
             AlbumDetailViewModel.Factory(activity.application, args.albumId)
         )[AlbumDetailViewModel::class.java]
+
         viewModel.album.observe(viewLifecycleOwner) {
             it.apply {
                 try {
@@ -52,7 +54,25 @@ class AlbumDetailFragment : Fragment() {
                         .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
                         .into(binding.albumDetailImage)
                 } catch (ex: Exception) {}
-                binding.albumDetailName.text = it.name
+                binding.albumDetailName.text = it.name + ":"+ it.description
+                binding.albumDetailPerformersTitle.text = "Performers:"
+                binding.albumDetailTracksTitle.text = "Tracks:"
+
+                // Load performer data
+                val recyclerView: RecyclerView = binding.performerDetailRv
+                val manager = LinearLayoutManager(context)
+                recyclerView.layoutManager = manager
+                val performerAdapter = PerformerAdapter(it.performers)
+                recyclerView.adapter = performerAdapter
+
+                // Load track data
+                val recyclerViewTrack: RecyclerView = binding.trackDetailRv
+                val managerTrack = LinearLayoutManager(context)
+                recyclerViewTrack.layoutManager = managerTrack
+                val trackAdapter = TrackAdapter(it.tracks)
+                recyclerViewTrack.adapter = trackAdapter
+
+                it.releaseDate
             }
         }
         viewModel.eventNetworkError.observe(viewLifecycleOwner) {
