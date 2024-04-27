47508cce16420df63a092fa6514d10195945b7e8
cczapatat <c.zapatat@uniandes.edu.co>
2024-04-26 23:43:17 -0500

    Pruebas Expresso Ver albumnes, filtrar, lista vacia, seleccionar un item

---
+++ app/src/androidTest/java/miso4203/mobile/app/vinilos/LoginAlbumsFilterNotExistsAny.kt
@@ -0,0 +1,93 @@
+package miso4203.mobile.app.vinilos
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class LoginAlbumsFilterNotExistsAny {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Test
+    fun loginAlbumsFilterNotExistsAny() {
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btn_visitor_login), withText("Visitor"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.albumsRv),
+                withParent(withParent(withId(R.id.nav_host_fragment_activity_main))),
+                isDisplayed()
+            )
+        )
+        recyclerView.check(matches(isDisplayed()))
+
+        val searchAutoComplete = onView(
+            allOf(
+                withId(R.id.searchView),
+
+                isDisplayed()
+            )
+        )
+        searchAutoComplete.perform(click())
+        searchAutoComplete.perform(typeText("notExists1234"),closeSoftKeyboard())
+
+        val recyclerView2 = onView(
+            allOf(
+                withId(R.id.albumsRv),
+                withParent(withParent(withId(R.id.nav_host_fragment_activity_main))),
+                isDisplayed()
+            )
+        )
+        recyclerView2.check(doesNotExist())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
